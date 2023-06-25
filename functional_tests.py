62e3c435a5f042cb22bf8e3df04101cb9b08bcc6
DemonJoystick <jonamkariuki@gmail.com>
2023-06-25 16:44:13 +0300

    refractoring

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import unittest
 
@@ -14,21 +15,23 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http:/localhost:8000')
         self.assertIn('To-Do',self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text 
+        header_text = self.browser.find_element(By.ID,'h1').text
+        #find_element(By.ID, ‘id’)
+        #find_element(By.NAME, ‘name’)
+        #find_element(By.XPATH, ‘xpath’)
         self.assertIn('To-Do', header_text)
         
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.test == '1: Buy peacock feathers' for row in rows))
+        time.sleep(2)
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.NAME,'tr')
         self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows),
-        "New to-do item did not appear in table"
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"  
         )
 
         #self.fail('Finish the test!')
